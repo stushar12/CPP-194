050daeb1538799c781c103df6bcb8a6a1d0f479b
Tushar Saxena <stushar345@gmail.com>
2021-03-01 21:16:20 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+struct BstNode 
+{
+	int data; 
+	BstNode* left;
+	BstNode* right;
+};
+
+BstNode* GetNewNode(int data) 
+{
+	BstNode* newNode = new BstNode();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+BstNode* Insert(BstNode* root,int data) 
+{
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+
+
+
+int main() 
+{
+	BstNode* root = NULL;  // Creating an empty tree
+
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+
+}
